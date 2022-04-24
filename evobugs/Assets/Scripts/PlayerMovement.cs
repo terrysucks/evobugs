9deb7ebccc7dfed6d861e248f9923486e4294aff
Koogleblitz <richardtobing@outlook.com>
2022-04-24 15:56:31 -0700

    enemies now have health, starve function makes them lose health over time, they take damage upon contact with other bugs, still need to implement bullet damage add hp boost with eating food and other enemies

--- evobugs/Assets/Scripts/PlayerMovement.cs
+++ evobugs/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,10 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 moveDirection;
 
+    [SerializeField]   //<-- makes the fields visible in the unity inspector
+    private int points = 1000;
+    private bool hit = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,4 +41,23 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(moveDirection.x*moveSpeed, moveDirection.y*moveSpeed);
     }
+
+
+//[+] ----------- Code for Taking Damage-------------[+]
+//NOTE: make sue "is trigger" box is checked in the 'Box Collider 2D" component in the inspector
+    IEnumerator HitBoxOff(){
+        hit= false;
+        yield return new WaitForSeconds(1.5f);
+    }
+    void OnTriggerEnter2D(Collider2D target){
+        if(target.tag == "enemy"){
+            if(hit){ 
+                points--;
+            }
+        }
+    }
+//[+]------------------------------------------------[+]
+
+
+
 }
--- evobugs/Assets/Scripts/enemyScripts/enemyFollow.cs
+++ evobugs/Assets/Scripts/enemyScripts/enemyFollow.cs
@@ -2,19 +2,97 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class enemyFollow : MonoBehaviour
 {
     public float speed;
     private Transform playerPos;
+    private Transform enemyPos;
+    public GameObject[] mobPositionArr;
+
+    public float distFromPlayer, distFromMob;
+
+    WaitForSeconds starveInterval= new WaitForSeconds(1);
+
+
+    //[+] points = health = damage = size multiplier
+    [SerializeField]   //<-- makes the fields visible in the unity inspector
+    private int points = 100;
+    private bool hit = true;
+    int counter = 0;
+
+
+
+ 
+
+
+
     void Awake()
     {
+        //player position
         playerPos = GameObject.FindWithTag("Player").transform;
+
+        //position of other enemies
+        enemyPos = GameObject.FindWithTag("enemy").transform;
+        //mobPositionArr = GameObject.FindGameObjectsWithTag("enemy");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(transform.position, playerPos.position) > 0.25f)
+        // if enemy is a certain distance from player/other enemy, move towards player/other enemy
+        distFromPlayer = Vector2.Distance(transform.position, playerPos.position);
+        distFromMob = Vector2.Distance(transform.position, enemyPos.position);
+
+
+        if(distFromPlayer > .25f)
             transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed*Time.deltaTime);
+        // else{
+        //     transform.position = Vector2.MoveTowards(transform.position, playerPos.position, -2*speed*Time.deltaTime);
+        // }
+
+        if((distFromMob > 3f)   )
+            transform.position = Vector2.MoveTowards(transform.position, enemyPos.position, speed*Time.deltaTime);
+        // else{
+        //     transform.position = Vector2.MoveTowards(transform.position, enemyPos.position, -5*speed*Time.deltaTime);
+        // }
+
+        if( counter > 10000){counter = 0;}
+        else{ counter++; }
+        
+        if ( (counter%50) == 0){
+            StartCoroutine(Starve());
+        }
+        
+        
     }
+
+    //[+] ----------- Code for Taking Damage-------------[+]
+//NOTE: make sue "is trigger" box is checked in the 'Box Collider 2D" component in the inspector
+    IEnumerator HitBoxOff(){
+        hit= false;
+        yield return new WaitForSeconds(1.5f);
+    }
+    void OnTriggerEnter2D(Collider2D target){
+        if(target.tag == "enemy"){
+            if(hit){ 
+                points--;
+            }
+        }
+    }
+
+
+    IEnumerator Starve(){
+        int i= 100;
+        while (i>0)
+        {
+            points--;
+            i--;
+            yield return starveInterval;
+        }
+        
+        
+    }
+//[+]------------------------------------------------[+]
+
 }
