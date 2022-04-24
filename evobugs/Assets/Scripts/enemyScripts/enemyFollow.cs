642f3316df600e4c9cd6885c5c294dce311ebc11
bleakrage <rb.machan@gmail.com>
2022-04-24 01:21:06 -0700

    Added Rigidbody and rotation to enemies

--- evobugs/Assets/Scripts/enemyScripts/enemyFollow.cs
+++ evobugs/Assets/Scripts/enemyScripts/enemyFollow.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class enemyFollow : MonoBehaviour
 {
     public float speed;
     private Transform playerPos;
+    private Rigidbody2D rb;
     void Awake()
     {
         playerPos = GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate for physics stuff
+    void FixedUpdate()
     {
-        if(Vector2.Distance(transform.position, playerPos.position) > 0.25f)
-            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed*Time.deltaTime);
+        //if(Vector2.Distance(transform.position, playerPos.position) > 0.25f)
+        //    transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed*Time.deltaTime);
+
+        // Face the player, then within the 0.25 distance threshold, advance toward player via Rigidbody2D
+        transform.right  = transform.position - playerPos.transform.position;
+        if (Vector2.Distance(transform.position, playerPos.position) > 0.25f)
+        {
+            rb.velocity = transform.right * -speed;
+        }
     }
 }
