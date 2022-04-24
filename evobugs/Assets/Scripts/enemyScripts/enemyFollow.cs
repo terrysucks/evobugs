c9f133b09fc2d6b5c9e857f65641ee2d1f91bf6d
Theron Myers <tgmyers5@gmail.com>
2022-04-24 14:24:06 -0700

    enemies face player :D

--- evobugs/Assets/Scripts/enemyScripts/enemyFollow.cs
+++ evobugs/Assets/Scripts/enemyScripts/enemyFollow.cs
@@ -14,7 +14,24 @@ public class enemyFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
         if(Vector2.Distance(transform.position, playerPos.position) > 0.25f)
             transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed*Time.deltaTime);
+
+        Vector2 facingDirection = playerPos.position - transform.position;
+        float aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg - 90f;
+        transform.eulerAngles = new Vector3(
+            transform.eulerAngles.x,
+            transform.eulerAngles.y,
+            aimAngle - 90
+        );    
     }
 }
